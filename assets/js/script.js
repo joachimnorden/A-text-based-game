4d1ddbac34ac0be971ab65c632de54ecfbc476c4
joachimnorden <joachim.norden@gmail.com>
2021-12-05 20:47:24 +0000

    Add functions and state variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 // Get the text and button elements
 const textElement = document.getElementById('game-text')
 const optionElement = document.getElementById('game-options')
+
+// the state variable will keep track on the inventory
+let state = {}
+
+function startGame() {
+    // Starting the game and set the state and options to where it is supposed to be
+
+}
+
+function showText(textIndex) {
+
+}
+
+function selectOption(option) {
+
+}
+
+startGame()
